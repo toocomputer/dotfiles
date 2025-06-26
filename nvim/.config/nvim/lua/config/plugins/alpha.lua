18cb8d90312429eb349ace5a05ed7c52726b6eb4
toocomputer <2061889+toocomputer@users.noreply.github.com>
2025-06-26 19:46:32 -0400

    finish plugin setup

--- nvim/.config/nvim/lua/config/plugins/alpha.lua
+++ nvim/.config/nvim/lua/config/plugins/alpha.lua
@@ -1,8 +1,38 @@
 return {
   {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-  };
+    "goolord/alpha-nvim",
+    lazy = false,
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Set menu
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("f", "󰈞 > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+        dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+        dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+        dashboard.button("q", " > Quit NVIM", ":qa<CR>"),
+      }
+
+      -- Set footer
+      local lazy_stats = require("lazy").stats() -- Get Lazy.nvim stats
+      dashboard.section.footer.val = {
+        "First, solve the problem. Then write the code.",
+        " ",
+        "                                  - John Johnson",
+        " ",
+--        "             Plugins loaded: " .. lazy_stats.loaded .. " / " .. lazy_stats.count,
+      }
+
+      -- Send config to alpha
+      alpha.setup(dashboard.opts)
+
+      -- Disable folding on alpha buffer
+      vim.cmd([[
+        autocmd FileType alpha setlocal nofoldenable
+      ]])
+    end,
+  },
 }
+
