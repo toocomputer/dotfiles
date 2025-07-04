d39d5dfc20070f23d4a75bd8962061653809c51a
ekmag <195157449+ekmag@users.noreply.github.com>
2025-07-04 14:23:44 -0400

    open telescope in normal mode

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -10,18 +10,18 @@ return {
       require('telescope').setup {
         defaults = {
           layout_strategy = "vertical",
+          initial_mode = "normal", 
           file_ignore_patterns = {
             "node_modules/.*",
             ".git/.*"
           }
         },
-        -- pickers = {
-        --   find_files = {
-        --     theme = "ivy"
-        --   }
-        -- }
+        pickers = {
+          find_files = {
+            hidden = true
+          }               
+        }
       }
-
       vim.keymap.set("n", "<leader>ff", require('telescope.builtin').find_files)
       vim.keymap.set("n", "<leader>fd", function()
         require('telescope.builtin').find_files {
