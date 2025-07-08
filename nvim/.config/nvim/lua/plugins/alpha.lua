75c67dc213cab3afa567b7753a840a2812061dd1
toocomputer <2061889+toocomputer@users.noreply.github.com>
2025-07-08 19:17:29 -0400

    remove menu option

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -15,7 +15,7 @@ return {
         dashboard.button("n", "  > New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("f", "󰱽  > Find file", ":cd $HOME/Projects | Telescope find_files<CR>"),
         dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-        dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | wincmd k | pwd<CR>"),
+      --dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | wincmd k | pwd<CR>"),
         dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
       }
 
